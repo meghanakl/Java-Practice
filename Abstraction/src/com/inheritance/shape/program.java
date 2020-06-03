8930fcee2703ac69091ce39e690ea189fc202ca7
meghanaraokl <meghanaraokl@gmail.com>
2020-06-03 20:07:16 +0530

    commit

--- Abstraction/src/com/inheritance/shape/program.java
+++ Abstraction/src/com/inheritance/shape/program.java
@@ -3,8 +3,11 @@ package com.inheritance.shape;
 public class program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
-}
+	public static void getShapeWithMaxArea() {
+		
+		
+	}
+	}
+
