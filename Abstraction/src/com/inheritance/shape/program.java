60b260cbe443e386829f5b2433137dd2cb250e2d
meghanaraokl <meghanaraokl@gmail.com>
2020-06-04 14:57:11 +0530

    commit

--- Abstraction/src/com/inheritance/shape/program.java
+++ Abstraction/src/com/inheritance/shape/program.java
@@ -5,8 +5,21 @@ public class program {
 	public static void main(String[] args) {
 	}
 
-	public static void getShapeWithMaxArea() {
+	public static  getShapeWithMaxArea() {
+	circle c = new circle();
+	c.radius = 10;
+	return c;
 		
+	rectangle r = new rectangle();
+	r.length = 10;
+	r.width=6;
+	return r;	
+		
+	square s = new square();
+	s.length = 5;
+	return s;
+		
+	
 		
 	}
 	}
