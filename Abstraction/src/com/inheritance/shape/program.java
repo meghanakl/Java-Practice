058fa7e3c29dbbe6375b93fe44c41787f5d38c63
meghanaraokl <meghanaraokl@gmail.com>
2020-06-04 15:41:00 +0530

    commit

--- Abstraction/src/com/inheritance/shape/program.java
+++ Abstraction/src/com/inheritance/shape/program.java
@@ -4,21 +4,21 @@ public class program {
 
 	public static void main(String[] args) { 
 		shape s =	getShapeWithMaxArea();
-		System.out.println("The maximum area contain shape is"+s);
+		System.out.println("The maximum area contain shape is "+s);
 	    double b=s.calculateArea();
-	    System.out.println("The area of biggest shape is"+b);
+	    System.out.println("The area of biggest shape is "+b);
 	}
 
 	public static  shape getShapeWithMaxArea() {
-	//circle c = new circle();
-	//c.radius = 10;
-	//return c;
+	circle c = new circle();
+	c.radius = 10;
+	return c;
 	
 		
-	rectangle r = new rectangle();
-	r.length = 10;
-	r.width=6;
-	return r;	
+	//rectangle r = new rectangle();
+   //r.length = 10;
+	//r.width=6;
+  //return r;	
 		
 	//square s = new square();
 	//s.length = 5;
