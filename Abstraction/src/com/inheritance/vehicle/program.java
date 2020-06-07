e2b30ea12a371c62a7ea69d1b30a7fb3d8def44d
meghanaraokl <meghanaraokl@gmail.com>
2020-06-07 13:49:10 +0530

    commit

--- Abstraction/src/com/inheritance/vehicle/program.java
+++ Abstraction/src/com/inheritance/vehicle/program.java
@@ -6,11 +6,17 @@ public class program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+ start();
 
 	}
-
+	 public static void start() {
+		 car c = new car();
+		 c.start();
+		
+		 bus b = new bus();
+		 b.start();
 	
-	
-	
+		 bike k = new bike();
+		 k.start();
 	}
 }
