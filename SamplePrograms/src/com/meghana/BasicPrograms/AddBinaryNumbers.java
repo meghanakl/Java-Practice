44252a3a182965f0468a9730fe36c14bcbaf6cec
meghanaraokl <meghanaraokl@gmail.com>
2020-08-09 00:19:31 +0530

    commit

---
+++ SamplePrograms/src/com/meghana/BasicPrograms/AddBinaryNumbers.java
@@ -0,0 +1,10 @@
+package com.meghana.BasicPrograms;
+
+public class AddBinaryNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SamplePrograms/src/com/meghana/BasicPrograms/LeapNumber.java
@@ -0,0 +1,24 @@
+package com.meghana.BasicPrograms;
+
+import java.util.Scanner;
+
+public class LeapNumber {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a = s.nextInt();
+		 CheckLeapNumber(a);
+	}
+
+	public static void CheckLeapNumber(int number) {
+		if (number % 100 == 0 && number % 400 == 0) {
+			System.out.println("The given " + number + " is leap number");
+		} else {
+			System.out.println("The given " + number + " is not a leap number");
+
+		}
+	}
+}
