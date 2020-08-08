07bd2bc3947eb22524084b199d42fd305894f043
meghanaraokl <meghanaraokl@gmail.com>
2020-08-08 23:53:21 +0530

    commit

--- SamplePrograms/src/com/meghana/string/operation/StringConcatination.java
+++ SamplePrograms/src/com/meghana/string/operation/StringConcatination.java
@@ -6,17 +6,23 @@ public class StringConcatination {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("Enter the first string");
+	System.out.println("Enter the number string");
 	Scanner s = new Scanner(System.in);
-	String String1 = s.nextLine();
+	String[] String = new String[ s.nextInt()];
+	s.nextLine();
 	
-	System.out.println("Enter the first string");
-	Scanner s1 = new Scanner(System.in);
-	String String2 = s.nextLine();
+	
+	for (int i=0;i<=String.length;i++) {
+		String[i] = s.nextLine();
+	}
+
 	}
 
 	public static String Concate(String s1, String s2) {
 
-		return s1 + s2;
+		return  s1 + s2;
+		
+		
+	 
 	}
 }
