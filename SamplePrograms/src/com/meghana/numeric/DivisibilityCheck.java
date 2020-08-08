17802690d76b754fc32c10b314614171595f8248
meghanaraokl <meghanaraokl@gmail.com>
2020-08-08 15:37:34 +0530

    commit

--- SamplePrograms/src/com/meghana/numeric/DivisibilityCheck.java
+++ SamplePrograms/src/com/meghana/numeric/DivisibilityCheck.java
@@ -1,5 +1,29 @@
 package com.meghana.numeric;
 
+import java.util.Scanner;
+
 public class DivisibilityCheck {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter a number");
+
+		Scanner s = new Scanner(System.in);
+		int number = s.nextInt();
+
+		if (number % 7 == 0 && number % 120 == 0) {
+			System.out.println("The number is divisible by both 7 and 120");
+		}
+		else if (number % 7 == 0) {
+			System.out.println("The number is divisible by only by 7");
+
+		}
+		else if (number % 120 == 0) {
+			System.out.println("The number is divisible by only by 120");
+		}
+
+		else {
+			System.out.println("The number is not divisible by 7 and 120");
+		}
+	}
 }
---
+++ SamplePrograms/src/com/meghana/string/operation/StringConcatination.java
@@ -0,0 +1,15 @@
+package com.meghana.string.operation;
+
+public class StringConcatination {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Meghana";
+		String s2 = "Rao";
+	}
+
+	public static String Concate(String s1, String s2) {
+
+		return s1 + s2;
+	}
+}
