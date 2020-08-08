8df74a2deaa772f74f9a514d0b165409fab26d62
meghanaraokl <meghanaraokl@gmail.com>
2020-08-08 15:55:32 +0530

    commit

--- SamplePrograms/src/com/meghana/string/operation/StringConcatination.java
+++ SamplePrograms/src/com/meghana/string/operation/StringConcatination.java
@@ -1,11 +1,18 @@
 package com.meghana.string.operation;
 
+import java.util.Scanner;
+
 public class StringConcatination {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s1 = "Meghana";
-		String s2 = "Rao";
+	System.out.println("Enter the first string");
+	Scanner s = new Scanner(System.in);
+	String String1 = s.nextLine();
+	
+	System.out.println("Enter the first string");
+	Scanner s1 = new Scanner(System.in);
+	String String2 = s.nextLine();
 	}
 
 	public static String Concate(String s1, String s2) {
