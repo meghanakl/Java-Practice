2485a08018ae7aa6d360a4ca2683a4be824ddad2
meghanaraokl <meghanaraokl@gmail.com>
2020-08-09 21:19:40 +0530

    commit

--- SamplePrograms/src/com/meghana/BasicPrograms/objectCount.java
+++ SamplePrograms/src/com/meghana/BasicPrograms/objectCount.java
@@ -2,18 +2,28 @@ package com.meghana.BasicPrograms;
 
 public class objectCount {
 
-	public static  int NoOfObjectsCreated;
+	public static  int NoOfObjectsCreated=124;
 	public String Name;
 	public int size=0;
 
 	public objectCount(String N) {
+		int n=0;
+		n=n+1;
 		this.Name=N;
 		NoOfObjectsCreated= NoOfObjectsCreated+1;
 		size= size+1;
 				}
 	
 	
+	public int GetANumber(){
+	int n=10;
+		return n;
+	}
 	
+	public int GetSize(){
+		return this.size= NoOfObjectsCreated;
+			
+		}
 	
 	}
 	
--- SamplePrograms/src/com/meghana/BasicPrograms/objectCountMain.java
+++ SamplePrograms/src/com/meghana/BasicPrograms/objectCountMain.java
@@ -4,26 +4,39 @@ public class objectCountMain {
           
 	public static void main(String[] args) {
 		objectCount o1 = new objectCount("o1");
-		objectCount o2= new objectCount("o2");
-		objectCount o3 = new objectCount("o3");
-
-		System.out.println("Number of object created is " + o1.NoOfObjectsCreated);
-		System.out.println("Number of object created is " + o2.NoOfObjectsCreated);
-		System.out.println("Number of object created is " + o3.NoOfObjectsCreated);
-		
+		o1.size=124;
 		System.out.println("Name of the object is"+o1.Name);
 		System.out.println("size of the object is"+o1.size);
-		System.out.println("Number of object created is"+o1.NoOfObjectsCreated);
-		
+		  System.out.println("Number of object created is"+o1.NoOfObjectsCreated);
+			
+		objectCount o2= new objectCount("o2");
 		System.out.println("Name of the object is"+o2.Name);
 		System.out.println("size of the object is"+o2.size);
-		System.out.println("Number of object created is"+o2.NoOfObjectsCreated);
-		
-		System.out.println("Name of the object is"+o3.Name);
+		  System.out.println("Number of object created is"+o2.NoOfObjectsCreated);
+			
+			
+		objectCount o3 = new objectCount("o3");
 		System.out.println("size of the object is"+o3.size);
+		System.out.println("Name of the object is"+o3.Name);
 		System.out.println("Number of object created is"+o3.NoOfObjectsCreated);
-	}
+		
 
+		System.out.println("Number of object created is " + o1.NoOfObjectsCreated);
+   	    System.out.println("Number of object created is " + o2.NoOfObjectsCreated);
+		System.out.println("Number of object created is " + o3.NoOfObjectsCreated);
+		
+
+		System.out.println("size of o1 is"+o1.GetSize());
+     
+		
+       System.out.println("size of o1 is"+o2.GetSize());
+       
+		System.out.println("size of o1 is"+o3.GetSize());
+	
+	System.out.println(o1.GetANumber());
+
+
+}
 }
 
 
