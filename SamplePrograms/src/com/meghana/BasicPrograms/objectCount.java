2f82eb891eca43e67d1bc2b0457f455051bfd9ae
meghanaraokl <meghanaraokl@gmail.com>
2020-08-09 20:44:14 +0530

    commit

--- SamplePrograms/src/com/meghana/BasicPrograms/objectCount.java
+++ SamplePrograms/src/com/meghana/BasicPrograms/objectCount.java
@@ -2,14 +2,14 @@ package com.meghana.BasicPrograms;
 
 public class objectCount {
 
-	public static int NoOfObjectsCreated;
-	
+	public static  int NoOfObjectsCreated;
+	public String Name;
+	public static int size=0;
 
-	public objectCount() {
-		
-		
+	public objectCount(String N) {
+		this.Name="N";
 		NoOfObjectsCreated= NoOfObjectsCreated+1;
-		
+		size= size+1;
 				}
 	
 	
--- SamplePrograms/src/com/meghana/BasicPrograms/objectCountMain.java
+++ SamplePrograms/src/com/meghana/BasicPrograms/objectCountMain.java
@@ -3,11 +3,25 @@ package com.meghana.BasicPrograms;
 public class objectCountMain {
           
 	public static void main(String[] args) {
-		objectCount o = new objectCount();
-		objectCount o1 = new objectCount();
-		objectCount o2 = new objectCount();
+		objectCount o = new objectCount("o");
+		objectCount o1 = new objectCount("o1");
+		objectCount o2 = new objectCount("o2");
 
-		System.out.println("Number of object created is " + objectCount.NoOfObjectsCreated);
+		System.out.println("Number of object created is " + o.NoOfObjectsCreated);
+		System.out.println("Number of object created is " + o1.NoOfObjectsCreated);
+		System.out.println("Number of object created is " + o2.NoOfObjectsCreated);
+		
+		System.out.println("Name of the object is"+o.Name);
+		System.out.println("size of the object is"+o.size);
+		System.out.println("Number of object created is"+o.NoOfObjectsCreated);
+		
+		System.out.println("Name of the object is"+o1.Name);
+		System.out.println("size of the object is"+o1.size);
+		System.out.println("Number of object created is"+o1.NoOfObjectsCreated);
+		
+		System.out.println("Name of the object is"+o2.Name);
+		System.out.println("size of the object is"+o2.size);
+		System.out.println("Number of object created is"+o2.NoOfObjectsCreated);
 	}
 
 }
