70e3385cf3d862d804e833c28fb2b4abcc9a40e5
meghanaraokl <meghanaraokl@gmail.com>
2020-08-09 20:51:34 +0530

    comit

--- SamplePrograms/src/com/meghana/BasicPrograms/objectCount.java
+++ SamplePrograms/src/com/meghana/BasicPrograms/objectCount.java
@@ -4,10 +4,10 @@ public class objectCount {
 
 	public static  int NoOfObjectsCreated;
 	public String Name;
-	public static int size=0;
+	public int size=0;
 
 	public objectCount(String N) {
-		this.Name="N";
+		this.Name=N;
 		NoOfObjectsCreated= NoOfObjectsCreated+1;
 		size= size+1;
 				}
--- SamplePrograms/src/com/meghana/BasicPrograms/objectCountMain.java
+++ SamplePrograms/src/com/meghana/BasicPrograms/objectCountMain.java
@@ -3,17 +3,13 @@ package com.meghana.BasicPrograms;
 public class objectCountMain {
           
 	public static void main(String[] args) {
-		objectCount o = new objectCount("o");
 		objectCount o1 = new objectCount("o1");
-		objectCount o2 = new objectCount("o2");
+		objectCount o2= new objectCount("o2");
+		objectCount o3 = new objectCount("o3");
 
-		System.out.println("Number of object created is " + o.NoOfObjectsCreated);
 		System.out.println("Number of object created is " + o1.NoOfObjectsCreated);
 		System.out.println("Number of object created is " + o2.NoOfObjectsCreated);
-		
-		System.out.println("Name of the object is"+o.Name);
-		System.out.println("size of the object is"+o.size);
-		System.out.println("Number of object created is"+o.NoOfObjectsCreated);
+		System.out.println("Number of object created is " + o3.NoOfObjectsCreated);
 		
 		System.out.println("Name of the object is"+o1.Name);
 		System.out.println("size of the object is"+o1.size);
@@ -22,7 +18,12 @@ public class objectCountMain {
 		System.out.println("Name of the object is"+o2.Name);
 		System.out.println("size of the object is"+o2.size);
 		System.out.println("Number of object created is"+o2.NoOfObjectsCreated);
+		
+		System.out.println("Name of the object is"+o3.Name);
+		System.out.println("size of the object is"+o3.size);
+		System.out.println("Number of object created is"+o3.NoOfObjectsCreated);
 	}
 
 }
+
 
