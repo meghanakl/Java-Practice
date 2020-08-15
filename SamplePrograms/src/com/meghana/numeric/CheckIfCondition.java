c3e455fa993587f41c32c4b22aadf0c2a333b6d2
meghanaraokl <meghanaraokl@gmail.com>
2020-08-15 17:21:26 +0530

    commit

---
+++ SamplePrograms/src/com/meghana/numeric/CheckIfCondition.java
@@ -0,0 +1,21 @@
+package com.meghana.numeric;
+
+public class CheckIfCondition {
+
+	public static void main(String[] args) {
+
+		if (true) 
+		{
+			System.out.println("hello");
+		}
+
+		else if (false) 
+		{
+			System.out.println("Hai");
+		} 
+		else 
+		{
+			System.out.println("Bai");
+		}
+	}
+}
